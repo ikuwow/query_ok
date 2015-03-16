cd3d63bbb1b34a8bcc811686e1a26231aac38424
Ikuo Degawa <degawa@ikuwow.com>
2015-03-16 23:41:03 +0900

    permalinkからcategoryを削除
    entryに変更。わかりやすいかな。

--- config.rb
+++ config.rb
@@ -9,7 +9,7 @@ activate :blog do |blog|
   # blog.prefix = "blog"
 
   # blog.permalink = "{year}/{month}/{day}/{title}.html"
-  blog.permalink = "{category}/{title}.html"
+  blog.permalink = "entry/{title}.html"
   # Matcher for blog source files
   # blog.sources = "{year}-{month}-{day}-{title}.html"
   blog.sources = "posts/{year}-{month}-{day}-{title}.html"
