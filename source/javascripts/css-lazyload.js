e9b0efad24c8846aa462d62d983876ed5c496ff2
Ikuo Degawa <degawa@ikuwow.com>
2015-06-20 17:53:42 +0900

    Remove unwanted querystrings

--- source/javascripts/css-lazyload.js
+++ source/javascripts/css-lazyload.js
@@ -1,21 +1,27 @@
 (function(d){
 
-    lazyload_css = function(filename) {
+    lazyload_css = function(filename,add_querystring) {
 
         if (filename.substr(-4,4)!=='.css') {
             filename = filename+'.css';
         }
-        var date = new Date();
-        var param = date.getFullYear().toString()+(date.getMonth()+1).toString()+date.getDate().toString();
+
         var c = d.createElement('link');
         c.type = 'text/css';
         c.rel = 'stylesheet';
-        c.href = filename+'?date='+param;
+
+        if (add_querystring===true) {
+            var date = new Date();
+            var param = date.getFullYear().toString()+(date.getMonth()+1).toString()+date.getDate().toString();
+            c.href = filename+'?date='+param;
+        } else {
+            c.href = filename;
+        }
         var s = d.getElementsByTagName('head')[0];
         s.appendChild(c);
     };
 
-    lazyload_css('/stylesheets/highlight');
+    lazyload_css('/stylesheets/highlight',true);
     lazyload_css('//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css');
 
 })(document);
