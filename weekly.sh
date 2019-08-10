1f57bd701e9f2f59d9b5f97d2d301f9ef856f21a
Ikuo Degawa <degawa@ikuwow.com>
2019-08-10 12:28:14 +0900

    Initialize weekly tweet compilation article

--- weekly.sh
+++ weekly.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-bundle exec middleman article "weekly-$(date -d '1 week ago' +%Y%m%d)"
+week_start=$(date -d '1 week ago' +%Y%m%d)
+week_end=$(date -d '1 day ago' +%Y%m%d)
+
+article_title="ツイートまとめ $week_start~$week_end"
+article_title_english="Weekly tweet compilation $week_start"
+
+output=$(bundle exec middleman article "$article_title_english" -t "Weekly" | tee /dev/tty)
+article_file=$(echo "$output" | awk '{print $NF}')
+
+sed -i "s/$article_title_english/$article_title/" "$article_file"
+
 open "https://twilog.org/ikuwow/date-$(date -d '1 week ago' +%y%m%d)/asc"
+vimr "$article_file"
