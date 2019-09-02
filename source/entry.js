920887339c2ec0a59b2604eaa64cb9f44a363121
Ikuo Degawa <degawa@ikuwow.com>
2019-09-02 21:57:12 +0900

    Load fontawesome by svg

--- source/entry.js
+++ source/entry.js
@@ -1,1 +1,8 @@
 console.log('This is index.js, imported by webpack bundle.');
+
+// Font awesome
+// see: https://fontawesome.com/how-to-use/with-the-api/setup/getting-started
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faTag } from '@fortawesome/free-solid-svg-icons';
+library.add(faTag);
+dom.i2svg();
