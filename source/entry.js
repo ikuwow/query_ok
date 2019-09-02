7e701e9fc06f37f21e53f3321aaffbea2377a6b0
Ikuo Degawa <degawa@ikuwow.com>
2019-09-02 20:46:34 +0900

    Minify bundle

--- source/entry.js
+++ source/entry.js
@@ -1,3 +1,4 @@
 console.log('This is index.js, imported by webpack bundle.');
 
-import '@fortawesome/fontawesome-free/js/all';
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/solid';
