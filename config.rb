1f331005c9c8cc0adf1b4c812d15570f73fee9f9
Ikuo Degawa <degawa@ikuwow.com>
2021-06-03 22:35:47 +0900

    Fix subtitle

--- config.rb
+++ config.rb
@@ -6,7 +6,7 @@ Time.zone = 'Tokyo'
 
 set :url_root, 'https://queryok.ikuwow.com'
 set :blog_title, 'Query OK.'
-set :blog_subtitle, 'ikuwowの技術/ガジェット/内省なブログ'
+set :blog_subtitle, 'ikuwowの技術/ガジェット/内省'
 
 activate :blog do |blog|
   blog.permalink = 'entry/{title}.html'
