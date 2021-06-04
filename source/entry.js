cd1e3d4854239d44b4baa1406af6a766fe8d564a
Ikuo Degawa <degawa@ikuwow.com>
2021-06-04 18:32:01 +0900

    Install @fortawesome/free-brands-svg-icons

--- source/entry.js
+++ source/entry.js
@@ -2,7 +2,15 @@
 // see: https://fontawesome.com/how-to-use/with-the-api/setup/getting-started
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faTag } from '@fortawesome/free-solid-svg-icons';
-library.add(faTag);
+import { faTwitter, faGithub, faFacebook, faInstagram } from '@fortawesome/free-brands-svg-icons';
+
+library.add(
+  faTag,
+  faTwitter,
+  faGithub,
+  faFacebook,
+  faInstagram
+);
 dom.i2svg();
 
 import './stylesheets/babylink.scss'
