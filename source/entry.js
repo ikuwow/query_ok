b6cd6ae77cbf3ef24e943e5b0bdd057dd112368d
Ikuo Degawa <ikuwow@users.noreply.github.com>
2021-07-04 10:54:37 +0900

    Remove fontawesome packages (#321)
    * Remove fontawesome packages
    
    * Install fontawesome-free
    
    * Import fontawesome-free
    
    * Add brands
    
    * Add solid icons

--- source/entry.js
+++ source/entry.js
@@ -1,16 +1,1 @@
-// Font awesome
-// see: https://fontawesome.com/how-to-use/with-the-api/setup/getting-started
-import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { faTag } from '@fortawesome/free-solid-svg-icons';
-import { faTwitter, faGithub, faFacebookSquare, faInstagram } from '@fortawesome/free-brands-svg-icons';
-
-library.add(
-  faTag,
-  faTwitter,
-  faGithub,
-  faFacebookSquare,
-  faInstagram
-);
-dom.i2svg();
-
 import './stylesheets/babylink.scss'
