0f65785429e4984347ed3726e118fd63833b889d
Ikuo Degawa <degawa@ikuwow.com>
2021-07-04 10:33:56 +0900

    Import fontawesome-free

--- source/critical.js
+++ source/critical.js
@@ -1,3 +1,4 @@
 import 'purecss/build/pure-min.css';
 import 'purecss/build/grids-responsive-min.css';
 import './stylesheets/layout.scss';
+import '@fortawesome/fontawesome-free/scss/fontawesome.scss';
